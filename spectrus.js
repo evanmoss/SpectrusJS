90067cf0bf662981aeff55efb7dc1e4a698a490a
Evan Moss <evanmoss@umich.edu>
2015-12-27 17:45:26 -0500

    Fixing resample

--- spectrus.js
+++ spectrus.js
@@ -642,7 +642,7 @@ module.exports = function() {
 	Vec.prototype.resample = function(n) {
 		var v = new Vec(this.type(), n), idx = 0, m = this.size();
 		for ( var i = 0; i < n; i++ ) {
-			var j = Math.floor(Math.random() * n--);
+			var j = Math.floor(Math.random() * m);
 			v.set(i, this.at(j));
 		}
 		return v;
